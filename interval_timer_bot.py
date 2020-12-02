9419fc79b55c220dd14971629c8242d406a1b7f7
Moritz Wuerich <muerek@hotmail.de>
2020-12-02 20:49:10 +0000

    Add voice channel joining.

--- interval_timer_bot.py
+++ interval_timer_bot.py
@@ -19,7 +19,7 @@ async def on_ready():
 
 @bot.command(name='hello', help='Responds with a hello message to show bot is up.')
 async def greeting(context):
-    await context.send('Hi there!')
+    await context.send('Hello there!')
 
 @bot.command(name='start', help='Starts a timer with a specified configuration.')
 async def start_timer(context, repetitions: int, work: int, rest: int):
@@ -30,7 +30,7 @@ async def start_timer(context, repetitions: int, work: int, rest: int):
         timer = IntervalTimer(repetitions, work, rest)
         VoiceAnnouncer(context, timer)
         timer.start()
-        await context.send(f'{repetitions} repetitions of {work} seconds work and {rest} seconds break, comin\' right up!')
+        await context.send(f'Alright, {repetitions} repetitions of {work} seconds work and {rest} seconds break, comin\' right up!')
     
 @bot.command(name='stop', help='Stops the timer currently running.')
 async def stop_timer(context):
@@ -39,7 +39,20 @@ async def stop_timer(context):
         timer.stop()
         await context.send('The timer was stopped.')
     else:
-        await context.send('There was no timer to stop, but let\'s still call this a success?')
+        await context.send('There was no timer to stop, but let\'s still call this a success, shall we?')
+
+@bot.command(name='voice', help='Instructs the bot to join the voice channel you are in.')
+async def join_voice(context: commands.Context):
+    # Not sure how to do this nicely, in other languages I would use the null-conditional operator on voice
+    voice_channel = context.author.voice.channel if context.author.voice is not None else None
+    if voice_channel is None:
+        await context.send('Oh my, I do not know where to go. Please join a voice channel so I can follow you.')
+        return
+    await voice_channel.connect()
+
+@bot.command(name='leave-voice', help='Instructs the bot to leave its voice channel.')
+async def leave_voice(context: commands.Context):
+    pass
 
 
 bot.run(os.getenv('BOT_TOKEN'))
